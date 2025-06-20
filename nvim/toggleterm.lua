8d59f302730c0b91e82bc8e17f63448d92e51265
Christian Lund <holmgaard1997@gmail.com>
2025-06-20 14:19:57 +0200

    Update toggleterm.lua

--- nvim/toggleterm.lua
+++ nvim/toggleterm.lua
@@ -9,6 +9,7 @@ return {
       { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle Floating Terminal" },
       { "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Toggle Horizontal Terminal" },
       { "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Toggle Vertical Terminal" },
+      { "<esc><esc>", [[<C-\><C-n>]], mode = "t" },
     },
     opts = {
       open_mapping = [[<c-t>]],
